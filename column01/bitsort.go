707c5deacb10323900e0129eed0a5992e5467190
Yuuki Furuyama <addsict@gmail.com>
2018-02-18 15:47:51 +0900

    const

--- column01/bitsort.go
+++ column01/bitsort.go
@@ -6,9 +6,15 @@ import (
 	"strconv"
 )
 
+const (
+	MAX_NUMBER        = 10000
+	BITFLAG_SIZE      = (MAX_NUMBER / 64) + 1 // 64bit
+	WORKING_MEM_BYTES = 1024
+)
+
 func BitSort(input io.Reader, output io.Writer) error {
-	bitflags := make([]uint64, 256)
-	reader := bufio.NewReaderSize(input, 1024)
+	bitflags := make([]uint64, BITFLAG_SIZE)
+	reader := bufio.NewReaderSize(input, WORKING_MEM_BYTES)
 	for {
 		// read
 		read, err := reader.ReadBytes('\n')
@@ -33,7 +39,7 @@ func BitSort(input io.Reader, output io.Writer) error {
 
 	// write
 	var i uint64
-	for i = 0; i < 64*256; i++ {
+	for i = 0; i < MAX_NUMBER; i++ {
 		sliceIdx := i / 64
 		bitIdx := i % 64
 		if ((bitflags[sliceIdx] >> bitIdx) & 1) == 1 {
